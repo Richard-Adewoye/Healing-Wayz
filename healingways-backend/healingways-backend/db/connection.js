41febb82d631fb73c360944ba01e2a441af17865
Richard Adewoye <richardadewoye031@gmail.com>
2026-08-23 22:43:53 +0100

    removed the folders that were not concerned with this project such as the html and claude generated backend

--- healingways-backend/healingways-backend/db/connection.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const Database = require('better-sqlite3');
-
-const DB_PATH = process.env.DB_PATH || path.join(__dirname, 'healingways.db');
-const db = new Database(DB_PATH);
-db.pragma('journal_mode = WAL');
-db.pragma('foreign_keys = ON');
-
-module.exports = db;
