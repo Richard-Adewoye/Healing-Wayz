683825e3f476452d596b7520b9f80b5a89ceb6ad
Richard Adewoye <richardadewoye031@gmail.com>
2026-08-23 22:39:56 +0100

    developed an animated loading page for the web app

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import { AnimatedLogoLoader } from './components/AnimatedLogoLoader';
 import HeroSection from './components/HeroSection';
 import SectionTwo from './components/SectionTwo';
 import SectionThree from './components/SectionThree';
@@ -11,8 +17,25 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Increased duration to 6000ms (6 seconds) to allow full app initialization
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 10000); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main className="min-h-screen">
+    <main className="relative min-h-screen">
+      {/* Animated Loading Overlay */}
+      <AnimatePresence mode="wait">
+        {isLoading && <AnimatedLogoLoader key="page-loader" />}
+      </AnimatePresence>
+
+      {/* Main Page Content */}
       <Navbar />
       <HeroSection />
       <SectionTwo />
